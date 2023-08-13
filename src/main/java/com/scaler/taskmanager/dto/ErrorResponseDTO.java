a98376c195dc789a93e805982b9faa3c04742f2a
Kaushal127 <87750260+Kaushal127@users.noreply.github.com>
2023-08-13 18:48:27 +0530

    update files added

---
+++ src/main/java/com/scaler/taskmanager/dto/ErrorResponseDTO.java
@@ -0,0 +1,14 @@
+package com.scaler.taskmanager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorResponseDTO {
+    private String error;
+}
--- src/main/java/com/scaler/taskmanager/services/TaskService.java
+++ src/main/java/com/scaler/taskmanager/services/TaskService.java
@@ -3,19 +3,24 @@ package com.scaler.taskmanager.services;
 import com.scaler.taskmanager.entities.TaskEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Pattern;
+
 @Service
 public class TaskService {
     private ArrayList<TaskEntity> tasks = new ArrayList<>() ;
     private  int taskID=1 ;
+    private final SimpleDateFormat deadLineFormatter = new SimpleDateFormat("yyyy-MM-DD");
 
-    public TaskEntity addTask(String title , String description , String deadline){
+    public TaskEntity addTask(String title , String description , String deadline) throws ParseException {
         TaskEntity task = new TaskEntity() ;
         task.setId(taskID);
         task.setTitle(title);
         task.setDescription(description);
-    //    task.setDeadline(new Date(deadline));
+        task.setDeadline(deadLineFormatter.parse(deadline));
         task.setCompleted(false);
         tasks.add(task);
         taskID++;
@@ -31,6 +36,22 @@ public class TaskService {
             }
         }
         return  null ;
+    }
+    public TaskEntity updateTask(int id ,  String description , String deadLine , Boolean completed) throws ParseException{
+        TaskEntity task = getTasksById(id) ;
+        if (task==null){
+            return null ;
+        }
+        if (description!=null){
+            task.setDescription(description);
+        }
+        if (deadLine!=null){
+            task.setDeadline(deadLineFormatter.parse(deadLine));
+        }
+        if (completed!=null){
+            task.setCompleted(completed);
+        }
+        return task ;
     }
 }
 
